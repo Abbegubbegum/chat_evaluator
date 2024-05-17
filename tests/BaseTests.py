e69e4b073fcfe7760c47b784f743cd4213a43691
Albin Norback <albin.nojback@gmail.com>
2024-05-17 10:54:58 +0200

    Reformat

--- tests/BaseTests.py
+++ tests/BaseTests.py
@@ -1,14 +1,17 @@
 from abc import ABC, abstractmethod
 from typing import Union
+
+
 class MessageTest(ABC):
-  @abstractmethod
-  def test(self, message: str, previous_messages: list[str]) -> bool:
-    assert False, "This method must be implemented in a subclass"
+    @abstractmethod
+    def test(self, message: str, previous_messages: list[str]) -> bool:
+        assert False, "This method must be implemented in a subclass"
+
 
 class GameTest(ABC):
-  @abstractmethod
-  def test(self, message_history: list[str]) -> bool:
-    assert False, "This method must be implemented in a subclass"
+    @abstractmethod
+    def test(self, message_history: list[str]) -> bool:
+        assert False, "This method must be implemented in a subclass"
 
 
 TestType = Union[MessageTest, GameTest]
